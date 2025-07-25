941421c4dd816cbb791205a63f1b8af3bb0a8591
JaehyeokLim <217686834+JaehyeokLim@users.noreply.github.com>
2025-07-25 15:30:30 +0900

    📝 docs: 공통 로깅 및 예외 클래스 추가
    - 로깅 유틸 클래스 Logger 추가
    - 404 응답 처리를 위한 커스텀 예외 정의 (PageNotFoundException)

--- src/main/java/com/jaehyeoklim/wasrestboard/httpserver/exception/PageNotFoundException.java
+++ src/main/java/com/jaehyeoklim/wasrestboard/httpserver/exception/PageNotFoundException.java
@@ -1,5 +1,10 @@
 package main.java.com.jaehyeoklim.wasrestboard.httpserver.exception;
 
+/**
+ * 요청한 경로에 해당하는 페이지가 없을 때 던지는 예외.
+ *
+ * <p>HTTP 404 응답을 표현할 때 사용됨.</p>
+ */
 public class PageNotFoundException extends RuntimeException {
     public PageNotFoundException(String message) {
         super(message);
--- src/main/java/com/jaehyeoklim/wasrestboard/util/Logger.java
+++ src/main/java/com/jaehyeoklim/wasrestboard/util/Logger.java
@@ -1,4 +1,14 @@
 package main.java.com.jaehyeoklim.wasrestboard.util;
 
-public class Logger {
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public abstract class Logger {
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss,SSS");
+
+    public static void log(Object obj) {
+        String time = LocalTime.now().format(formatter);
+        System.out.printf("%s [%20s] %s\n", time, Thread.currentThread().getName(), obj);
+    }
 }
